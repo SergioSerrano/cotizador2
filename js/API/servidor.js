6dc2cf9a2fe53b6cb3fcb81fe3f8e91d6081ac76
Sergio Serrano Perez <pepes17@hotmail.com>
2013-10-25 15:54:21 -0500

    resuelto

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -3,29 +3,9 @@
 
 
 
-<<<<<<< HEAD
-function nuevoAjax()
-{
-        var xmlhttp=false;
-        try
-        {
-                // Creacion del objeto AJAX para navegadores no IE
-                xmlhttp=new ActiveXObject("Msxml2.XMLHTTP");
-        }
-        catch(e)
-        {
-                try
-                {
-                        // Creacion del objet AJAX para IE
-                        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-                }
-                catch(E)
-                {
-                        if (!xmlhttp && typeof XMLHttpRequest!="undefined") xmlhttp=new XMLHttpRequest();
-                }
-        }
-        return xmlhttp;
-=======
+
+
+
 function nuevoAjax() {
 	var xmlhttp = false;
 	try {
@@ -40,7 +20,7 @@ function nuevoAjax() {
 		}
 	}
 	return xmlhttp;
->>>>>>> dd
+
 }
 
 function enviarDatos(nom, tel, email) {
