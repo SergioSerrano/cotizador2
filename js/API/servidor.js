6694cafbcd8e70993184d53e12229aa74afb88ca
Sergio Serrano Perez <pepes17@hotmail.com>
2013-11-15 09:20:58 -0600

    error con descripcion

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -377,6 +377,31 @@ alert(fileSystem.fullpath);
 
     function fail(error) {
         alert(error.code);
+	    
+	    var msg = '';
+
+  switch (error.code) {
+    case FileError.QUOTA_EXCEEDED_ERR:
+      msg = 'QUOTA_EXCEEDED_ERR';
+      break;
+    case FileError.NOT_FOUND_ERR:
+      msg = 'NOT_FOUND_ERR';
+      break;
+    case FileError.SECURITY_ERR:
+      msg = 'SECURITY_ERR';
+      break;
+    case FileError.INVALID_MODIFICATION_ERR:
+      msg = 'INVALID_MODIFICATION_ERR';
+      break;
+    case FileError.INVALID_STATE_ERR:
+      msg = 'INVALID_STATE_ERR';
+      break;
+    default:
+      msg = 'Unknown Error';
+      break;
+  };
+
+  alert('Error: ' + msg);
     }
 
 
