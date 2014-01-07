5089359897ec3f949a33e4871d5841bd50b2a593
Sergio Serrano Perez <pepes17@hotmail.com>
2014-01-07 15:42:01 -0600

    acentos

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -372,7 +372,10 @@ function gotFileWriter(writer)
 	//	'width': 170,
 	//	'elementHandlers': specialElementHandlers
 	//});
-	doc.text( 20,20'Av. Hermanos S&eacute;rdan N&deg; 717 Col. Sn Rafael Poniente, CP 72029 Puebla, Pue. M&eacute;xico. 01 (222) 2668330 ext 113 ferias&#64;victorinox.com.mx');
+	doc.setFontSize(16);
+    doc.text(40, 30, 'Hola, Felíz Año Nuevo! áéíóú'); 
+	doc.text( 20,20,'Av. Hermanos Sérdan N° 717 Col. Sn Rafael Poniente, CP 72029 Puebla, Pue. México. ');
+	doc.text(50,20,'01 (222) 2668330 ext 113 ferias&#64;victorinox.com.mx');
 	//doc.save('ejemplo'+$('#no_cliente').val()+'.pdf');
 	// alert(filevar); 
 	writer.write(doc.output());
